1c430a8cda28f34dad249b532d5ee509b39ef3ed
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-06 13:51:27 -0300

    feat(component): PomodoroTimer passing tests

--- src/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { usePomodoro } from 'hooks/usePomodoro';
+import formatCount from 'utils/formatCount';
+
+const formattedCountDefault = {
+  hours: '0',
+  minutes: '00',
+  seconds: '00',
+};
 
 const PomodoroTimer: React.FC = () => {
+  const [formattedCount, setFormattedCount] = useState(formattedCountDefault);
   const { count } = usePomodoro();
+
+  useEffect(() => {
+    setFormattedCount(formatCount(count));
+  }, [count]);
 
   return (
     <div>
-      <span data-testid="minutes">00</span>
+      {count?.hours > 0 && (
+        <>
+          <span data-testid="hours">{formattedCount.hours}</span>
+          <span>:</span>
+        </>
+      )}
+      <span data-testid="minutes">{formattedCount.minutes}</span>
       <span>:</span>
-      <span data-testid="seconds">00</span>
+      <span data-testid="seconds">{formattedCount.seconds}</span>
     </div>
   );
 };
