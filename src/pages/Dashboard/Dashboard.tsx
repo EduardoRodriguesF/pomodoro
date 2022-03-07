0e6b5266e5f8fd5cd8ee830f36b6ac029c9c5ff2
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-07 01:06:29 -0300

    feat(Dashboard): populated page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,13 @@
+import PomodoroButton from 'components/PomodoroButton';
+import PomodoroTimer from 'components/PomodoroTimer';
 import React from 'react';
 
 const Dashboard: React.FC = () => (
-  <h1>Pomodoro</h1>
+  <>
+    <h1>Pomodoro</h1>
+    <PomodoroTimer />
+    <PomodoroButton />
+  </>
 );
 
 export default Dashboard;
