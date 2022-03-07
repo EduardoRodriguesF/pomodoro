e03f231369e54e75a4f3e447c264c982239ea212
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-06 21:48:17 -0300

    feat(component): PomodoroButton

--- src/components/PomodoroButton/PomodoroButton.tsx
+++ src/components/PomodoroButton/PomodoroButton.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { FiPause, FiPlay } from 'react-icons/fi';
+import { usePomodoro } from 'hooks/usePomodoro';
 
-const PomodoroButton: React.FC = () => <div />;
+const PomodoroButton: React.FC = () => {
+  const { startTimer, pauseTimer, isRunning } = usePomodoro();
+
+  const handleClick = useCallback(() => {
+    if (isRunning) pauseTimer();
+    else startTimer();
+  }, [isRunning, startTimer, pauseTimer]);
+
+  return (
+    <button type="button" onClick={handleClick}>
+      {isRunning ? <FiPause data-testid="pause-icon" /> : <FiPlay data-testid="play-icon" />}
+    </button>
+  );
+};
 
 export default PomodoroButton;
