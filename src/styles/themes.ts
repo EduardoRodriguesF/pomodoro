51ca2251cf9f4119a47879ec159a633ad9231af0
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-10 12:05:24 -0300

    styles: themes object file

---
+++ src/styles/themes.ts
@@ -0,0 +1,18 @@
+const themes = {
+  white: {
+    primary: '#DF3A36',
+    text: '#1b2530',
+    background: '#fff',
+    bodyBackground: '#f4f4f4',
+    border: '#e6e6e6',
+  },
+  dark: {
+    primary: '#DF3A36',
+    text: '#fff',
+    background: '#1b2530',
+    bodyBackground: '#000',
+    border: '#222',
+  },
+};
+
+export default themes;
