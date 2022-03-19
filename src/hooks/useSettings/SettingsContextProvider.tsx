fa7e1fb90c10559ea27ab60ee0ae759bc9e421a7
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-19 16:13:58 -0300

    feat(useSettings): use of local storage

--- src/hooks/useSettings/SettingsContextProvider.tsx
+++ src/hooks/useSettings/SettingsContextProvider.tsx
@@ -1,24 +1,14 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { ITime } from 'types';
+import getStoredSetting from 'utils/getStoredSetting';
 import { SettingsContext } from '.';
 
 const DEFAULT_VALUE = {
-  focusTime: {
-    hours: 0,
-    minutes: 25,
-    seconds: 0,
-  },
-  breakTime: {
-    hours: 0,
-    minutes: 5,
-    seconds: 0,
-  },
-  longBreakTime: {
-    hours: 0,
-    minutes: 15,
-    seconds: 0,
-  },
-  pauseAfterCycle: true,
-  longBreakInterval: 5,
+  focusTime: getStoredSetting('focusTime') as ITime,
+  breakTime: getStoredSetting('breakTime') as ITime,
+  longBreakTime: getStoredSetting('longBreakTime') as ITime,
+  pauseAfterCycle: getStoredSetting('pauseAfterCycle') as boolean,
+  longBreakInterval: getStoredSetting('longBreakInterval') as number,
 };
 
 const SettingsContextProvider: React.FC = ({ children }) => {
@@ -37,6 +27,29 @@ const SettingsContextProvider: React.FC = ({ children }) => {
   const togglePauseAfterCycle = useCallback(() => {
     setPauseAfterCycle(!pauseAfterCycle);
   }, [pauseAfterCycle]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      'focusTime@Pomodoro.Settings',
+      JSON.stringify(focusTime),
+    );
+    localStorage.setItem(
+      'breakTime@Pomodoro.Settings',
+      JSON.stringify(breakTime),
+    );
+    localStorage.setItem(
+      'longBreakTime@Pomodoro.Settings',
+      JSON.stringify(longBreakTime),
+    );
+    localStorage.setItem(
+      'pauseAfterCycle@Pomodoro.Settings',
+      JSON.stringify(pauseAfterCycle),
+    );
+    localStorage.setItem(
+      'longBreakInterval@Pomodoro.Settings',
+      JSON.stringify(longBreakInterval),
+    );
+  }, [focusTime, breakTime, longBreakTime, longBreakInterval, pauseAfterCycle]);
 
   const settings = useMemo(
     () => ({
