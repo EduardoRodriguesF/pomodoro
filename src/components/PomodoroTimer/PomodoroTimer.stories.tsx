0f28530eb6cf167d14f6e58e8f75b26670484d9a
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-19 21:54:49 -0300

    stories(PomodoroTimer): added PomodoroRing as a subcomponent

--- src/components/PomodoroTimer/PomodoroTimer.stories.tsx
+++ src/components/PomodoroTimer/PomodoroTimer.stories.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { ComponentMeta } from '@storybook/react';
 import { PomodoroContextProvider, usePomodoro } from 'hooks/usePomodoro';
+import PomodoroRing from 'components/PomodoroRing';
 import PomodoroTimer from '.';
 
 export default {
   title: 'PomodoroTimer',
   component: PomodoroTimer,
+  subcomponents: { PomodoroRing },
   decorators: [
     Story => (
       <PomodoroContextProvider>
