445881abdf33e48fa78043945b6a52757160df92
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-21 20:40:18 -0300

    refactor(Button): props as optional

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,8 +7,8 @@ const Button: React.FC<IButtonProps> = ({
   children,
   title,
   variant = 'primary',
-  disabled,
-  round,
+  disabled = false,
+  round = false,
   ...rest
 }) => (
   <Wrapper
--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,8 +1,8 @@
 interface IButtonProps {
   title: string;
-  variant: 'primary' | 'secondary' | 'text';
-  disabled: boolean;
-  round: boolean;
+  variant?: 'primary' | 'secondary' | 'text';
+  disabled?: boolean;
+  round?: boolean;
 }
 
 export type { IButtonProps };
